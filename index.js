61e8e452cd65fe199ccd3dee7c22deb7d4139422
Jose Enrique Robaina <jrp@Joses-MacBook-Pro.local>
2026-03-01 22:25:56 -0600

    Added Chalk colored console output

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import chalk from "chalk";
 
-console.log(chalk.blue("=== Chalk Project ==="));
-console.log(chalk.green("Repository successfully created!"));
-console.log(chalk.yellow("This message uses Chalk colors."));
-console.log(chalk.red.bold("JavaScript + Node + GitHub"));
+console.log(chalk.blue.bold("=== Chalk Project ==="));
+
+console.log(
+    chalk.green("✔ Repository successfully created!")
+);
+
+console.log(
+    chalk.yellow("✔ This message uses Chalk colors.")
+);
+
+console.log(
+    chalk.magenta.bold("Node.js + Git + GitHub working together!")
+);
