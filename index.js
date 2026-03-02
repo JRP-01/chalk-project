586de96994357c6a741e311805f785bae4635bf9
Jose Enrique Robaina <jrp@Joses-MacBook-Pro.local>
2026-03-01 22:22:42 -0600

    Initial repository setup with Node project

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("=== Chalk Project ==="));
+console.log(chalk.green("Repository successfully created!"));
+console.log(chalk.yellow("This message uses Chalk colors."));
+console.log(chalk.red.bold("JavaScript + Node + GitHub"));
